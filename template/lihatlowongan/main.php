bd7856e22df22848931c1f0e43c6fab220e91c86
ilhamlq <ilhamlatulqadari@gmail.com>
2017-07-10 20:37:26 +0700

    Template

---
+++ template/lihatlowongan/main.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/tes.css">
+	<title>
+		SISTEM INFORMASI PENERIMAAN PEGAWAI
+	</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row"> 
+			<div class="col-md-12" id="atas">
+				<<p><a href="daftarkerja.html">Beranda</a></p>
+				<a href="index.html"><span class="glyphicon glyphicon-off" aria-hidden="true"></span> Log Out</a>
+				<a href="#"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> Notifikasi</a>
+				<a href="profilpelamar.html"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Profil</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12" id="navigasi">
+				<img src="img/untan.png">
+				<h1>SISTEM INFORMASI
+				<br>PENERIMAAN PEGAWAI</h1>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid" id="daftar">
+		<h1>Silahkan Pilih Pekerjaan yang Anda Inginkan</h1>
+		<div class="row">
+		  <div class="col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/regis.jpg" alt="">
+		      <div class="caption">
+		        <h3>Back-End Developer</h3>
+		        <p>Status:
+		        <br>- Dibuka
+		        <br>- Ditutup 28-01-2016
+		        </p>
+		        <a href="persyaratan.html" class="btn btn-info" role="button">Lihat Persyaratan</a>
+		      </div>
+		    </div>
+		  </div>
+	
+		  <div class="col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/regis.jpg" alt="">
+		      <div class="caption">
+		        <h3>Sales</h3>
+		        <p>Status:
+		        <br>- Dibuka
+		        <br>- Ditutup 28-01-2016
+		        </p>
+		        <a href="persyaratan.html" class="btn btn-info" role="button">Lihat Persyaratan</a>
+		      </div>
+		    </div>
+		  </div>
+	
+		  <div class="col-md-4">
+		    <div class="thumbnail">
+		      <img src="img/regis.jpg" alt="">
+		      <div class="caption">
+		        <h3>Resepsionis</h3>
+		        <p>Status:
+		        <br>- Dibuka
+		        <br>- Ditutup 28-01-2016
+		        </p>
+		        <a href="persyaratan.html" class="btn btn-info" role="button">Lihat Persyaratan</a>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	</div>
+
+	<div class="container-fluid" id="footer"> 
+		<div class="col-md-12">
+			&copy; Kelompok 13 
+			<p>Jln. Prof. Dr. H. Hadari Nawawi | Phone: +6255555555, Fax: (0256781237</p>
+		</div>
+	</div>
+
+</body>
+</html>
