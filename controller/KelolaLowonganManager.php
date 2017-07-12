222e7403fedbe1ec8abaaa9851f34c3bd40dbfbc
ariyudhaprawira <ariyudhaprawira@gmail.com>
2017-07-12 08:26:11 +0700

    Membuat Model, Controller dan view Kelola Lowongan.

---
+++ controller/KelolaLowonganManager.php
@@ -0,0 +1,32 @@
+<?php 
+	include 'Controller.php';
+	/**
+	* 
+	*/
+	class LowonganManager extends Controller
+	{
+		
+		public function setTambahLowongan()
+		{
+			# code...
+		}
+
+
+		public function setEditLowongan()
+		{
+			# code...
+		}
+
+
+		public function setDetailLowongan()
+		{
+			# code...
+		}
+
+
+		public function setHapusLowongan()
+		{
+			# code...
+		}
+	}
+ ?>
---
+++ controller/KelolaUserManager.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+* 
+*/
+include_once 'Controller.php';
+class KelolaUserManager extends Controller
+{
+	
+	public function setTambahUser()
+	{
+		
+	}
+
+	public function setDetailUser()
+	{
+		
+	}
+
+	public function setEditUser()
+	{
+		
+	}
+
+	public function setDeleteUser()
+	{
+		
+	}
+	
+	
+}
+	
+ ?>
