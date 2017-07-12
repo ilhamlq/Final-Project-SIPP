56ea2cfcc6d22fddc5c3d4203ccf8bb6a122f26e
khairunnisamasitah <khairunn223@gmail.com>
2017-07-12 14:12:47 +0700

    Penambahan Model dan View Kelola user Perusahaan

---
+++ view/KelolaUserPerusahaanUI.php
@@ -0,0 +1,47 @@
+<?php 
+
+/**
+* 
+*/
+
+include_once 'Model.php';
+class Lowongan extends Model
+{
+	public $id_manager;
+	public $id_ceo;
+	public $id_hrd;
+	public $username;
+	public $password;
+	public $nama_lengkap;
+	public $email;
+	public $tanggal_lahir;
+	public $alamat;
+	public $jenis_kelamin;
+	public $no_hp;
+	
+	public function tambahUser()
+	{
+		
+	}
+	
+	public function detailUser()
+	{
+		
+	}
+
+	public function editUser()
+	{
+		
+	}
+
+	public function deleteUser()
+	{
+		
+	}
+	
+
+}
+
+
+
+ ?>
